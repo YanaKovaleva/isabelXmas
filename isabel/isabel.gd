bb368f9e69e93c4620f99861ac8f2f477ed25474
YanaKovaleva <eeemailll73@gmail.com>
2025-12-29 12:43:08 -0500

    Pictures and movement
    Imported pictures, figured out 0 gravity movement (yay!!), set world boundaries so that the player doesn't walk off the screen or on to the walls. Up next is basic object interaction

---
+++ isabel/isabel.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	var direction1 = 0;
+	# Add the gravity.
+	#if not is_on_floor():
+	#	velocity += get_gravity() * delta
+	
+	# Handle jump.
+	if Input.is_action_pressed("up"):
+		direction1-=1;
+	if Input.is_action_pressed("down"):
+		direction1+=1;	
+	velocity.y = direction1 * SPEED;
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+#		Idea is that the body shouldn't get out of the room
+#func _on_world_area_body_exited(body: Node2D) -> void:
+#	Isabel.position = 110;
+
+
+func _on_left_boundary_body_entered(body: Node2D) -> void:
+	velocity.x = 5 * SPEED;
+	move_and_slide()
+
+
+func _on_right_boundary_body_entered(body: Node2D) -> void:
+	velocity.x = -5 * SPEED;
+	move_and_slide()
+
+func _on_top_boundary_body_entered(body: Node2D) -> void:
+	velocity.y = 15 * SPEED;
+	move_and_slide()
+
+func _on_bottom_boundary_body_entered(body: Node2D) -> void:
+	velocity.y = -15 * SPEED;
+	move_and_slide()
