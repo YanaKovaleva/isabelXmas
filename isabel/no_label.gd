9a7c22d224fc5fd65cae38a139fd636c4275a7c7
YanaKovaleva <eeemailll73@gmail.com>
2025-12-30 21:55:50 -0500

    Photos, songs, quiz
    Designed and imported the things for music and photos, designed and coded the quiz

---
+++ isabel/no_label.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_wrong_pressed() -> void:
+	show()
+	await get_tree().create_timer(2.0).timeout; #from godot forum
+	get_tree().change_scene_to_file("res://main.tscn");
--- isabel/wrong.gd
+++ isabel/wrong.gd
@@ -1,16 +1,11 @@
-extends Sprite2D
+extends TextureButton
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	show()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if (body.name == "gameIsabel"):
-		hide()
